937c9c1143b83cebdd12b7b8b61f1a85be52f300
Aldidas5 <79062492+Aldidas5@users.noreply.github.com>
2021-05-19 19:12:47 +0200

    Add files via upload

---
+++ Mastermind.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Mastermind{
+	public static void main(String[] args){
+		
+		//Computer generierte Zahl
+		int len = eingabe.length()
+		Random random = new Random();
+		random.nextInt(1000);
+		
+		Grenzwert = 10
+		Versuche = Counter
+		
+		//Spielerraten
+		
+		if (Versuche < Grenzwert)
+		Scanner input = new Scanner(System.in);
+		String Eingabe = input.next();
+		System.out.println("Eine Eingabe lautet" + Eingabe);
+		
+		
+		//Abgleich
+		
+		if (counterEingabe < Grenzwert);
+			System.out.println("Du hast noch " + x + "Versuche frei!")
+			
+		if (counterEingabe == Grenzwert);
+			System.out.println("Du hast bereits alle 10 Versuche benutzt")
+			
+		
+		
+		if (eingabe.charAt(0) == random.charAt(0));
+			System.out.println("Die erste Stelle ist richtig")
+		if (eingabe.charAt(1) == random.charAt(1));
+			System.out.println("Die zweite Stelle ist richtig")
+		if (eingabe.charAt(2) == random.charAt(2));
+			System.out.println("Die dritte Stelle ist richtig")		
+		if (eingabe.charAt(3) == random.charAt(3));	
+			System.out.println("Die letzte Stelle ist richtig")
+			System.out.println("Du hast es geschafft !")
+
+
+		else
+			System.out.println("Leider falsch")
+	}
+}
